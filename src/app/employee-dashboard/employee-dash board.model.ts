abf5341d7a4bdbeadffb0c2da9584ea26b3df0c6
JakovJonjić <73243731+Jakov69@users.noreply.github.com>
2022-03-28 00:16:08 +0200

    second

---
+++ src/app/employee-dashboard/employee-dash board.model.ts
@@ -0,0 +1,14 @@
+export class EmployeeModel{
+     id : number = 0 ;
+    firstName : string = '';
+    lastName : string = '';
+    email: string = '';
+    mobile: string = '';
+    salary : string = '';
+
+    sisanje: string='';
+    bojanje: string='';
+    lakiranje: string='';
+    
+    //name: string='';
+}
